9032a5a40c14a353e8d401f258178b9d43741c00
julia sheremet <juliajessicasher@gmail.com>
2018-07-12 21:44:23 -0700

    create component, action and reducer folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Login from './Login.jsx';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>app</h1>
+        <Login />
       </div>
     );
   }
---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Login(){
+  let loginStyles = {
+    fontWeight: 'bold',
+    fontFamily: 'monospace',
+    color: 'black',
+    fontSize: '28px'
+  }
+
+  return(
+    <div style={loginStyles}>
+      <h1>NURTURE</h1>
+        <button type="button">SIGN IN</button>
+        <button type="button">SIGN UP</button>
+      <p>Forgot your logins? Click Here!</p>
+
+    </div>
+  );
+}
+
+export default Login;
