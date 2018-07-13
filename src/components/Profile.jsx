a2f313a6e42474ebcf766cb6bc850ab84e02e0f6
julia sheremet <juliajessicasher@gmail.com>
2018-07-13 15:34:46 -0700

    Add a bg image for the profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import profileBgImg from './../assets/imgs/profile-bg.svg';
 
 function Profile(){
 
@@ -11,8 +12,15 @@ function Profile(){
       <Nav />
       <style jsx>{`
         .profile {
+          margin: 0;
+          padding: 0;
+          display: block;
           padding: 0 2rem;
           height: 562px;
+          background-image: url(${profileBgImg});
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: cover;
         }
       `}
       </style>
