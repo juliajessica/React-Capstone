bf6be0378641a65856d8d787cc183aaa81bd7edb
julia sheremet <juliajessicasher@gmail.com>
2018-07-13 13:29:46 -0700

    add css to plant search

--- src/components/PlantSearch.jsx
+++ src/components/PlantSearch.jsx
@@ -7,11 +7,29 @@ function PlantSearch(){
     <div>
       <div className="plant-Search">
         <h1>PlantSearch</h1>
+        <form>
+          <input type="text" placeholder="SEARCH:" className="search-input"></input>
+        </form>
       </div>
       <Nav/>
       <style jsx>{`
         .plant-Search{
           padding: 0 2rem;
+          height: 550px;
+        }
+        .search-input {
+          padding: .7rem;
+          font-size: 1rem;
+          border-radius: 5px;
+          margin: .4rem 0;
+          width: 20rem;
+          border: 0px solid;
+
+        }
+
+        input[type=text] {
+          color: white;
+          background-color: #E18472;
         }
       `}
       </style>
