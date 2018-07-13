eaea76a71756289172be16e271a5b33484b477e0
julia sheremet <juliajessicasher@gmail.com>
2018-07-13 15:18:34 -0700

    Add buttons to calendar add and calendar edit components

--- src/components/CalendarAdd.jsx
+++ src/components/CalendarAdd.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import { Link } from 'react-router-dom';
 
 function CalendarAdd(){
 
@@ -7,6 +8,10 @@ function CalendarAdd(){
     <div>
       <div className="calendar-add">
         <h3>Calendar Add</h3>
+          <div className="calendar-btns">
+            <Link to='/calendar-edit'><button type="button" className="edit-cal">EDIT</button></Link>
+            <Link to='/calendar-add'><button type="button" className="add-cal">ADD</button></Link>
+          </div>
 
       </div>
       <Nav/>
@@ -14,6 +19,36 @@ function CalendarAdd(){
         .calendar-add  {
           padding: 0 2rem;
           height: 562px;
+        }
+        .calendar-btns {
+          display: grid;
+          grid-gap: 5px;
+          grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+          grid-auto-rows: 75px;
+          grid-auto-flow: dense;
+
+        }
+        .edit-cal {
+          font-size: .8rem;
+          color: white;
+          width: 10rem;
+          background-color:#0A07DF;
+          border-radius: 5px;
+          padding: .5rem 0;
+          margin: .3rem 0;
+          border: 0px solid;
+          opacity: .8;
+        }
+        .add-cal {
+          font-size: .8rem;
+          color: white;
+          width: 10rem;
+          background-color:#EBAFA4;
+          border-radius: 5px;
+          padding: .5rem 0;
+          margin: .3rem 0;
+          border: 0px solid;
+          opacity: .8;
         }
 
       `}
--- src/components/CalendarEdit.jsx
+++ src/components/CalendarEdit.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import { Link } from 'react-router-dom';
 
 function CalendarEdit(){
 
@@ -7,13 +8,46 @@ function CalendarEdit(){
     <div>
       <div className="calendar-edit">
         <h3>Calendar Edit</h3>
-
+          <div className="calendar-btns">
+            <Link to='/calendar-edit'><button type="button" className="edit-cal">EDIT</button></Link>
+            <Link to='/calendar-add'><button type="button" className="add-cal">ADD</button></Link>
+          </div>
       </div>
       <Nav/>
       <style jsx>{`
         .calendar-edit {
           padding: 0 2rem;
           height: 562px;
+        }
+        .calendar-btns {
+          display: grid;
+          grid-gap: 5px;
+          grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+          grid-auto-rows: 75px;
+          grid-auto-flow: dense;
+
+        }
+        .edit-cal {
+          font-size: .8rem;
+          color: white;
+          width: 10rem;
+          background-color:#0A07DF;
+          border-radius: 5px;
+          padding: .5rem 0;
+          margin: .3rem 0;
+          border: 0px solid;
+          opacity: .8;
+        }
+        .add-cal {
+          font-size: .8rem;
+          color: white;
+          width: 10rem;
+          background-color:#EBAFA4;
+          border-radius: 5px;
+          padding: .5rem 0;
+          margin: .3rem 0;
+          border: 0px solid;
+          opacity: .8;
         }
       `}
       </style>
