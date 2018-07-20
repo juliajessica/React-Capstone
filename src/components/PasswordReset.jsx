3537a123349833dc9014a0f9f3d03f4d2d9a00ab
julia sheremet <juliajessicasher@gmail.com>
2018-07-20 11:57:40 -0700

    add input and button values to the password reset component

--- src/components/PasswordReset.jsx
+++ src/components/PasswordReset.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignInButton from './SignInButton';
+import { Link } from 'react-router-dom';
 
 function PasswordReset(){
 
@@ -7,16 +8,46 @@ function PasswordReset(){
     <div>
       <div className="password-reset">
         <h3>Password Reset</h3>
-        <SignInButton />
+        <div className="password-reset-input">
+          <input type="text" placeholder="EMAIL"></input>
+          <Link to=''><button type="button" className="reset-button">RESET PASSWORD</button></Link>
+        </div>
 
       </div>
       <style jsx>{`
         .password-reset {
           height: 565px;
         }
+        .password-reset-input {
+          display:flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 2rem;
+        }
+        .password-reset-input input{
+          padding: .7rem;
+          font-size: 1rem;
+          border-radius: 5px;
+          margin: .4rem 0;
+          width: 20rem;
+          border: 0px solid;
+        }
+        input[type=text] {
+          color: white;
+          background-color: #E18472;
+        }
+        .reset-button {
+          color: white;
+          width: 15rem;
+          background-color:#0A07DF;
+          border: 0px solid;
+          border-radius: 5px;
+          padding: .5rem 0;
+          margin: .3rem 0;
+          opacity: .8;
+        }
       `}
-      </style>
-
+    </style>
 
     </div>
   );
