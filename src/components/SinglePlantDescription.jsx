641473ed8055a8747a478fce633c8321793608e5
Student <example@example.com>
2018-07-24 09:13:23 -0700

    sadd jsx style and button to single plant description

--- src/components/SinglePlantDescription.jsx
+++ src/components/SinglePlantDescription.jsx
@@ -1,37 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import plantSVG from './../assets/imgs/pagelines.svg';
+//import plantSVG from './../assets/imgs/pagelines.svg';
 
 function SinglePlantDescription(props){
   console.log(props.description);
 
   return(
     <div>
-      <h1>plant description</h1>
-      <h2><img src={plantSVG} className="leaf-icon" alt="Icon of a plant"/>{props.name}</h2>
-      <p>Description: {props.description}</p>
-      <div className="plant-desc-img">
-        <img src={props.image} alt="image of plant" className="plant-desc-imgResize" />
-
-          <p>{props.maintenance}</p>
-          <p>{props.water}</p>
-          <p>{props.exposure}</p>
-          <p>{props.growthPeriod}</p>
-          <p>{props.bloom}</p>
-
-
+      <div className="flex-plant">
+      <h1 className="plant-name">{props.name}</h1>
+        <div className="plant-desc-img">
+          <img src={props.image} alt="image of plant" className="plant-desc-imgResize" />
+        </div>
       </div>
+      <p><span className="plant-properties">Description:</span> {props.description}</p>
+      <p><span className="plant-properties">Maintenance:</span> <img src={props.maintenance} alt="leaf icon" className="leaf-svg"/></p>
+      <p><span className="plant-properties">Water:</span> {props.water}</p>
+      <p><span className="plant-properties">Exposure:</span> {props.exposure}</p>
+      <p><span className="plant-properties">Growth Period:</span> {props.growthPeriod}</p>
+      <p><span className="plant-properties">Bloom:</span> {props.bloom}</p>
+      <button>ADD TO GARDEN</button>
+
       <style jsx>{`
+        .flex-plant {
+          display: flex;
+          justify-content: space-between;
+
+        }
+        .plant-name {
+          border-radius: 5;
+        }
         .plant-desc-img {
           display: block;
           overflow: hidden;
           height: 100px;
           width: 100px;
           box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
         }
         .plant-desc-imgResize {
           width: 120px;
         }
+        .plant-properties {
+          font-weight: bold;
+          color: #d69ea5;
+        }
+        .leaf-svg{
+          padding: .2rem;
+          width: 15px;
+        }
+
 
         `}
       </style>
